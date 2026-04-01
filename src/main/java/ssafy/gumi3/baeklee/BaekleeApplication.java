fb96e6dd98f06fcb009cd52931456aaa0ac5bdae
백성수 <bss011202@naver.com>
2026-04-01 15:20:51 +0900

    init: 프로젝트 생성

---
+++ src/main/java/ssafy/gumi3/baeklee/BaekleeApplication.java
@@ -0,0 +1,13 @@
+package ssafy.gumi3.baeklee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaekleeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaekleeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssafy/gumi3/baeklee/BaekleeApplicationTests.java
@@ -0,0 +1,13 @@
+package ssafy.gumi3.baeklee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaekleeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
